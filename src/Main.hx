4bd4456d8767161aeb83f111bfedab6935975816
obi3112 <drazeobi3112@gmail.com>
2024-02-08 20:58:16 -0600

    added player logic

--- src/Main.hx
+++ src/Main.hx
@@ -1,50 +1,29 @@
+package;
+
+/*
+ 
+* @author obi3112
+*/
+import Player;
 import haxegon.*;
+class Main 
 
-class Main {
-	var player = {
-		x: 120,
-		y: 120,
+{
+	public static final WINDOW_WIDTH = Gfx.screenwidth;
+	public static final WINDOW_HEIGHT = Gfx.screenheight;
+	
+	
+	public function new() 
+	{
+	
 	}
-  private var screen_col = 0xFF00FF; // Use the Col enum to specify the color or use Hex Code for the colors!
-  private var x: Int = 12; // Use Int to specify integer values
-  private var y: Int = 15;
-  private var width: Int = 50;
-  private var height: Int = 70;
-  private var color: Int = 4; // Use Int to specify integer values
-  final WINDOW_WIDTH:Int = 720;
-  final WINDOW_HEIGHT:Int = 490;
-  public function new() {
-    init();
-	update();
-  }
-
-  function playerMovement() {
-    if (Input.pressed(Key.LEFT)) {
-      player.x -= 10;
-    }
-    if (Input.pressed(Key.RIGHT)) {
-      player.x += 10;
-    }
-    if (Input.pressed(Key.UP)) {
-      player.y -= 10;
-    }
-    if (Input.pressed(Key.DOWN)) {
-      player.y += 10;
-    }
-    // Making sure the player doesn't get off the screen should be added here
-	if (player.x >= WINDOW_WIDTH) {
-		player.x = WINDOW_WIDTH;
-	//else if (player.x <= 0) {}
+	
+	private function init() {
 	}
-  }
-
-  function init() {
-    Gfx.resizescreen(0, 0); // Specify the screen size
-    Gfx.clearcolor = screen_col;
-  }
-
-  function update() {
-    Gfx.fillbox(player.x, player.y, width, height, color);
-    playerMovement();
-  }
+	
+	private function update() {
+		//instances
+		var player:Player = new Player();	
+	}
+	
 }
--- src/Player.hx
+++ src/Player.hx
@@ -1,15 +1,70 @@
 package;
 
-/**
- * ...
+/*
  * @author obi3112
+ * 
  */
-class Player 
+
+import haxegon.*;
+
+class Player
 {
 
-	public function new() 
-	{
-		PlayerMovement(
+	public function new() {
+		update();
+	}
+	
+	var WINDOW_WIDTH = Gfx.screenwidth;
+	var WINDOW_HEIGHT = Gfx.screenheight;
+	private static var player = {
+		x : 120,
+		y : 120,
+		width : 30,
+		height : 30,
+		col : Col.RED,
+		speed : 10,
+	};
+	
+	private function PlayerMovementLogic() {
+		//player movement
+		if (Input.pressed(Key.W)) {
+			player.y -= player.speed;
+		}
+		if (Input.pressed(Key.S)) {
+			player.y += player.speed;
+		}
+		if (Input.pressed(Key.A)) {
+			player.x -= player.speed;
+		}
+		if (Input.pressed(Key.D)) {
+			player.x += player.speed;
+		}
+		trace(Gfx.screenwidth);
+		//player collision that checks when the player goes off screen
+		
+		if (player.x >= Gfx.screenwidth - player.width) {
+			player.x = Gfx.screenwidth - player.width;
+		}
+		if (player.y >= Gfx.screenheight - player.height) {
+			player.y = Gfx.screenheight - player.height;
+		}
+		if (player.x <= 0) {
+			player.x = 0;
+		}
+		if (player.y <= 0) {
+			player.y = 0;
+		}	
+		
+	}
+	
+	private function init() {
+	
+	}
+	
+	private function update() {
+		Gfx.resizescreen(0, 0);
+		Gfx.fillbox(player.x, player.y, player.width, player.height, player.col);
+		PlayerMovementLogic();
 	}
 	
 }
